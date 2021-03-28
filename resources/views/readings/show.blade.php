8ae259e67f1408da0159cffc00ffe206fc4d7eed
Gregor Laan <laangregor@gmail.com>
2021-03-28 19:54:31 +0300

    Reading single view

--- resources/views/readings/show.blade.php
+++ resources/views/readings/show.blade.php
@@ -13,10 +13,22 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-white border-b border-gray-200 text-center flex">
 
-                    <p>Updated at: {{ $reading->updated_at }}</p>
-                    <p>Value: {{ $reading->value }}</p>
+                    <div class="w-full md:w-1/3">
+                        <p>Created at</p>
+                        <p class="text-4xl">{{ $reading->created_at }}</p>
+                    </div>
+
+                    <div class="w-full md:w-1/3">
+                        <p>Value</p>
+                        <p class="text-4xl">{{ $reading->value }}</p>
+                    </div>
+
+                    <div class="w-full md:w-1/3">
+                        <p>Updated at</p>
+                        <p class="text-4xl">{{ $reading->updated_at }}</p>
+                    </div>
 
                 </div>
             </div>
